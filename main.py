b201fa34d5c9de53c264fa2b88709e201a17c321
shohei-218 <95951450+shohei-218@users.noreply.github.com>
2021-12-13 23:29:58 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,98 +1,21 @@
-import cv2  # opencv-python==4.2.0.34
-import streamlit as st  # streamlit==0.61.0
-import tensorflow as tf  # tensorflow==2.2.0
-from tensorflow import keras
-
-
-def get_model():
-    model = keras.applications.MobileNetV2(include_top=True, weights="imagenet")
-    model.trainable = False
-    return model
-
-
-def get_decoder():
-    decode_predictions = keras.applications.mobilenet_v2.decode_predictions
-    return decode_predictions
-
-
-def get_preprocessor():
-    def func(image):
-        image = tf.cast(image, tf.float32)
-
-        image = tf.image.resize(image, (224, 224))
-        image = keras.applications.mobilenet_v2.preprocess_input(image)
-        image = tf.expand_dims(image, axis=0)
-        return image
-
-    return func
-
-
-class Classifier:
-    def __init__(self, top_k=5):
-        self.top_k = top_k
-        self.model = get_model()
-        self.decode_predictions = get_decoder()
-        self.preprocessor = get_preprocessor()
-
-    def predict(self, image):
-        image = self.preprocessor(image)
-        probs = self.model.predict(image)
-        result = self.decode_predictions(probs, top=self.top_k)
-        return result
-
-
-def main():
-    st.markdown("# Image Classification app using Streamlit")
-    st.markdown("model = MobileNetV2")
-    device = user_input = st.text_input("input your video/camera device", "0")
-    if device.isnumeric():
-        device = int(device)
-    cap = cv2.VideoCapture(device)
-    classifier = Classifier(top_k=5)
-    label_names_st = st.empty()
-    scores_st = st.empty()
-    image_loc = st.empty()
-
-    while cap.isOpened():
-        _, frame = cap.read()
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        result = classifier.predict(frame)
-        labels = []
-        scores = []
-        for (_, label, prob) in result[0]:
-            labels.append(f"{label: <16}")
-            s = f"{100*prob:.2f}[%]"
-            scores.append(f"{s: <16}")
-        label_names_st.text(",".join(labels))
-        scores_st.text(",".join(scores))
-        image_loc.image(frame)
-        if cv2.waitKey() & 0xFF == ord("q"):
-            break
-    cap.release()
-
-
-if __name__ == "__main__":
-    main()
-
-
-#from typing import Text
-#import streamlit as st
+from typing import Text
+import streamlit as st
 #import numpy as np
 #import pandas as pd
-#from PIL import Image
-#import time
+from PIL import Image
+import time
 
-#st.title('Streamlit 超入門')
+st.title('Streamlit 超入門')
 
-#st.write('interactive Widgets')
+st.write('interactive Widgets')
 
-#latest_iteration=st.empty()
-#bar=st.progress(0)
+latest_iteration=st.empty()
+bar=st.progress(0)
 
-#for i in range(100):
-#    latest_iteration.text(f'Iteration {i+1}')
-#    bar.progress(i+1)
-#    time.sleep(0.4)
+for i in range(100):
+    latest_iteration.text(f'Iteration {i+1}')
+    bar.progress(i+1)
+    time.sleep(0.4)
 
 
 #'done'
