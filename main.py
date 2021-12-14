c6ed9952c80a09a7c3f2503f49bffa8792604e85
shohei-218 <95951450+shohei-218@users.noreply.github.com>
2021-12-14 23:03:24 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,85 +1,61 @@
-from typing import Text
-import streamlit as st
-#import numpy as np
-#import pandas as pd
-from PIL import Image
-import time
-import
+import streamlit as st  # streamlit==0.61.0
+import numpy as np
+from keras.models import load_model
+from keras.preprocessing.image import ImageDataGenerator
+from keras.preprocessing import image
+import tensorflowjs as tfjs
+
+from streamlit_webrtc import webrtc_streamer
 
 
-st.title('バヤッシーのStreamlit 超入門')
+webrtc_streamer(key="example")
+
+
+st.title('Webカメラで遊ぼう！')
 
 st.write('interactive Widgets')
 
-latest_iteration=st.empty()
-bar=st.progress(0)
 
-for i in range(100):
-    latest_iteration.text(f'Iteration {i+1}')
-    bar.progress(i+1)
-    time.sleep(0.4)
-
-
-#'done'
-
-
-#left_column, right_column=st.beta_columns(2)
-
-#button = left_column.button('右カラムに文字を表示')
-#if button:
-#    right_column.write('ここは右カラムです')
-
-#expander1=st.expander('問い合わせ1')
-#expander1.write('問い合わせ回答')
-#expander1=st.expander('問い合わせ2')
-#expander1.write('問い合わせ回答2')
-#expander1=st.expander('問い合わせ3')
-#expander1.write('問い合わせ回答3')
+class VideoProcessor:
+    def __init__(self) -> None:
+        self.threshold1 = 100
+        self.threshold2 = 200
+    
+    def recv(self, frame):
+        img = frame.to_ndarray(format="bgr24")
+        
+        img = cv2.cvtColor(cv2.Canny(img, self.threshold1, self.threshold2), cv2.COLOR_GRAY2BGR)
+        
+        return av.VideoFrame.from_ndarray(img, format="bgr24")
 
 
+ctx = webrtc_streamer(key="example", video_processor_factory=VideoProcessor)
+if ctx.video_processor:
+    ctx.video_processor.threshold1 = st.slider("Threshold1", min_value=0, max_value=1000, step=1, value=100)
+    ctx.video_processor.threshold2 = st.slider("Threshold2", min_value=0, max_value=1000, step=1, value=200)
+
+#classes = ({0:'angry',1:'disgust',2:'fear',3:'happy',
+#          4:'sad',5:'surprise',6:'neutral'})
+
+#image_path='/Users/kambayashishouhei/desktop/koba_a.jpg'
 
 
-#text = st.text_input('あなたの趣味を教えてください')
-#condition = st.slider('あなたの今の調子は？',0,100,50)
+#img = image.load_img(image_path, grayscale=True , target_size=(64, 64))
+#img_array = image.img_to_array(img)
+#pImg = np.expand_dims(img_array, axis=0) / 255
 
-#'あなたの趣味:',text, 'です'
-#'コンディション:',condition
+#model_path = '/Users/kambayashishouhei/desktop/fer2013_mini_XCEPTION.110-0.65.hdf5'
 
+#emotions_XCEPTION = load_model(model_path, compile=False)
 
+#prediction = emotions_XCEPTION.predict(pImg)[0]
 
+##convert the model into tf.js model
+#save_path = '../nodejs/static/emotion_XCEPTION'
+#tfjs.converters.save_keras_model(emotions_XCEPTION, save_path)
+#print("[INFO] saved tf.js emotion model to disk..")
 
-#option = st.selectbox(
-#    'あなたが好きな数字を教えてください,',
-#    list(range(1,11))
-#)
-#'あなたの好きな数字は、', option, 'です。'
-
-#if st.checkbox('Show image'):
-#    img=Image.open('onomichi.jpg')
-#    st.image(img, caption='Shohei Kambayashi', use_column_width=True)
-
-
-
-#df = pd.DataFrame(
-#    np.random.rand(100, 2)/[50,50]+[35.69,139.70],
-#    columns=['lat', 'lon']
-#)
-#st.table(df.style.highlight_max(axis=0))
-
-#st.map(df)
-
-
-
-
-
-"""
-# 章
-## 節
-### 項
-
-```python
-import streamlit as st
-import numpy as np
-import pandas as pd
-```
-"""
+#top_indices = prediction.argsort()[-5:][::-1]
+#result = [(classes[i] , prediction[i]) for i in top_indices]
+#for x in result:
+#    print(x)
